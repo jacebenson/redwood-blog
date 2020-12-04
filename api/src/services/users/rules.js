cdd76c2deca555e3cd4596deb85faa3fd4634664
Jace Benson <jace.benson@gmail.com>
2020-12-04 11:52:32 -0600

    trying other mthod

--- api/src/services/users/rules.js
+++ api/src/services/users/rules.js
@@ -1,12 +1,47 @@
-
+/*
 import { rule as createTask } from './rules/create-task'
 import { rule as updatejacedcom } from './rules/updatejacedcom'
 import { rule as updatejacedcomfalse } from './rules/updatejacedcomfalse'
+import { rule as updatejacedcomcopy2 } from './rules/updatejacedcom copy 2'
+import { rule as updatejacedcomcopy3 } from './rules/updatejacedcom copy 3'
+import { rule as updatejacedcomcopy4 } from './rules/updatejacedcom copy 4'
+import { rule as updatejacedcomcopy5 } from './rules/updatejacedcom copy 5'
+import { rule as updatejacedcomcopy6 } from './rules/updatejacedcom copy 6'
+import { rule as updatejacedcomcopy7 } from './rules/updatejacedcom copy 7'
+import { rule as updatejacedcomcopy8 } from './rules/updatejacedcom copy 8'
+import { rule as updatejacedcomcopy9 } from './rules/updatejacedcom copy 9'
+import { rule as updatejacedcomcopy10 } from './rules/updatejacedcom copy 10'
+import { rule as updatejacedcomcopy11 } from './rules/updatejacedcom copy 11'
 
 let rulesList = [];
 rulesList.push(createTask)
 rulesList.push(updatejacedcom)
+rulesList.push(updatejacedcomcopy2)
+rulesList.push(updatejacedcomcopy3)
+rulesList.push(updatejacedcomcopy4)
+rulesList.push(updatejacedcomcopy5)
+rulesList.push(updatejacedcomcopy6)
+rulesList.push(updatejacedcomcopy7)
+rulesList.push(updatejacedcomcopy8)
+rulesList.push(updatejacedcomcopy9)
+rulesList.push(updatejacedcomcopy10)
+rulesList.push(updatejacedcomcopy11)
 rulesList.push(updatejacedcomfalse)
+*/
+
+let path = require('path')
+let basepath = './api/src/services/users/rules/'
+let files = [
+  "updatejacedcom.js",
+  "create-task"
+]
+let rulesList = [];
+files.forEach(function(file){
+  let rule = require(path.join(basepath, file));
+  console.log("rule", rule.rule);
+  rulesList.push(rule.rule);
+})
+
 
 rulesList.sort((a,b)=>a.order-b.order);
 
